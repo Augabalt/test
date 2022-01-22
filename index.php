6bb15c6a744897aeb23222964c2a604daa3f561c
Augabalt <auga.baltic@gmail.com>
2022-01-22 11:10:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 include 'Product.php';
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Scandiweb Test</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
